e459aade8e2e0a2fd84abc1d307b6c092b208c7e
Gokul Sadanandam <gokul.sadanandam@tatacommunications.com>
2019-01-15 16:06:07 +0530

    Schedule Section Added!

--- client/components/dashboard/dashboard.js
+++ client/components/dashboard/dashboard.js
@@ -7,7 +7,7 @@ angular.module('dashboard', ['ngAnimate', 'ngMaterial', 'ngMessages'])
     .factory('interceptor', function() {
         return {
             request: function(config) {
-                config.headers["Authorization"] = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjJhOTkzY2FkNmY2YTM5YWZlYWNhNmU4NzBlNWRjMDYxMjViOGFmYzcwMWIxNjQ4NzJhNTAyNDJmNDM2MzkwMDEwNDMwYTU1MDEwMWZkM2RiIn0.eyJhdWQiOiIyIiwianRpIjoiMmE5OTNjYWQ2ZjZhMzlhZmVhY2E2ZTg3MGU1ZGMwNjEyNWI4YWZjNzAxYjE2NDg3MmE1MDI0MmY0MzYzOTAwMTA0MzBhNTUwMTAxZmQzZGIiLCJpYXQiOjE1NDY4Njk0NTAsIm5iZiI6MTU0Njg2OTQ1MCwiZXhwIjoxNTQ4MTY1NDUwLCJzdWIiOiI1YTk0YTVjMC0xMGRhLTExZTktYTFmOS05MThhMzVhZjUwMDIiLCJzY29wZXMiOltdfQ.fRyy44SRQrNZ46U2_G7A9YLS-Fu5Pt4UMpbHS6qEoazYjwiMxmmYBOaYfX-taEgqGzK64bDdLPUL62No80A0eYdt7eYhUIqj5VJ1s5kA6Zt5BDuJr7_aKGaEntLoMagRx22dDC0pDYqfms4qV0nTP-Fpx2l338OtZNjavPSH0thndR03TsbfjIxtDDLyZLcjXhi4XWOGXmhd_MJDlGj1HQVLY2DYw8nfrRjcTzXe8x93NglUFazNLyO1ecBW36-BtEXkWuLIIBD0axvDTBac089g-G9ulkRps1kSkQjOCfAEMu0tEWvrYv3enw1_3a1KgXD-FVQ4ZJmPOhfdnUJfuUr5ezCilFlBx3fAtsn3HZeMUtR38Iq9g7fUfycIP4fhXyCVwvZk2UU0VvvKJtdghLjmYc-2ffwCyFiQtScw3dYXm0j6cJ44B9vRs-k1DSdqO7atqGvYVsePAkH0KD4LTzNn8ltWhsjja6xlzrFrm_uVdRsBwQLOeNCGTY5qndonSJanrKvz1dDkEoLNgrreO6Tb0mml-Lx-HlQtaG5ke7IBG60ZpIddBHFjqiC2F3etFJkT_uuyjDaMb6Xci3YPAMIdjTnI4aeMs8UbNNro-0fq7Ydd9XIH5Chhc7JbZiVVEkzaXkIEcp4VP1dg-_VWLXZ07AX8Bbx3ThY980POHv4"
+                config.headers["Authorization"] = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImQ5YTliZmIyNzBkZGM4YThmOTFjNWJmN2NlZTIzYzM0ODIzMmY0Y2NlZWNjNzk0OTNlYzQ1MzAwMDNmNTJiMzdlNmM1NzZmNzU2YzE4ZjRkIn0.eyJhdWQiOiIyIiwianRpIjoiZDlhOWJmYjI3MGRkYzhhOGY5MWM1YmY3Y2VlMjNjMzQ4MjMyZjRjY2VlY2M3OTQ5M2VjNDUzMDAwM2Y1MmIzN2U2YzU3NmY3NTZjMThmNGQiLCJpYXQiOjE1NDc0NzYzMjgsIm5iZiI6MTU0NzQ3NjMyOCwiZXhwIjoxNTQ4NzcyMzI4LCJzdWIiOiI1YTk0YTVjMC0xMGRhLTExZTktYTFmOS05MThhMzVhZjUwMDIiLCJzY29wZXMiOltdfQ.LfijeBC8TbE3RDG4jRw1Lq8WnbHtV4NFrHMBOurK-VltErGvCHUwKuPcq3J49hae-mFv8Gn6gj4H3HgJsFHAAfwIuZX9RdJcMXU7JU_7olmtGGL0oJv2MWdN0852G8WCU2fCgZ7klF-Cr8UwMV2kW-VFKWYxcSYD0TtywomRT2aERIoyQD_u8BCCqOm2XySwZbCP6x9bwmIk9opKnEYm3rqNslc-iv7s2-A7c_keBiiXxCcQMh2U5pp7mPQ9HCuLkhqeg4HMUH1RjySuevfWMPl5quVXtfPubUgWKKnBgmeGTi6L1FcFrQfshwpTZCLMi41JEexqihXewbrcZrEig8CJSXhee6TeodLLSkY6FBlr5rzjEdA9hs6cmBqTeA1YFc2qigFi9H4pHmG8WQduKXSxkzE9ZLvo5BARdsZi0jPIXu3nraCZ3f8pMkOxmIjqh3rBpdOjZXr0fAUUhmnAzru6MKG_5pugpKzzC-GQqXLKF6GSjyQM81cnoIOTRGWjQmfCAB6VCeQHKLjY8rS5KyjTdsUtalAeW3xb5HvrbNC_mNo5seCHfG31F0LyMOlgmR4xC_C5K7wtyGdIFODZUB19aCA1xbyYf1erozAWx6-RHjREqGcTEkS9z3WMznpVNucpa6HDref1GmYf9wm-EeKHa4SbU8bL-rzolnGMTts"
                 config.headers["referer-domain"] = "fwda.devship.freightbro.com"
                 return config
             }
@@ -49,13 +49,29 @@ angular.module('dashboard', ['ngAnimate', 'ngMaterial', 'ngMessages'])
                 url: url,
                 method: 'GET'
             }).then((response) => {
-                this.data.push(response)
+                // console.log(response.data.data[0])
+                if (!response.data.data.length) {
+                    this.data.push({
+                        "error": "Schedule Not Available"
+                    })
+                } else {
+                    this.data.push({
+                        "destinationPortName": response.data.data[0]["destination_port_name"],
+                        "transitTime": response.data.data[0]["transit_time"],
+                        "viaPort": response.data.data[0]["via_port"],
+                        "departureTime": response.data.data[0]["departure_time"],
+                        "carrierCode": response.data.data[0]["carrier_code"]
+                    })
+                }
             }, (error) => {
-                this.data.push({"error" : "Schedule Not Available"})
+                this.data.push({
+                    "error": "Schedule Not Available"
+                })
             })
         }
 
         this.getData = function() {
+            console.log(this.data)
             return this.data
         }
 
@@ -64,7 +80,8 @@ angular.module('dashboard', ['ngAnimate', 'ngMaterial', 'ngMessages'])
 
         $scope.options = ['Ascending', 'Descending']
         $scope.result = 'Ascending'
-        
+        $scope.result = []
+
         function isEmpty(data) {
             if (data.length) {
                 return data
@@ -72,11 +89,11 @@ angular.module('dashboard', ['ngAnimate', 'ngMaterial', 'ngMessages'])
             return data.length
         }
 
-        function timeConversion(data) {
+        $scope.timeConversion = function(data) {
             let pattern = /\d+\-\d+\-\d+/g;
             if (data != "NA") {
                 let date = new Date(data)
-                return "Expires on " + date.toLocaleString('en-us', {
+                return date.toLocaleString('en-us', {
                     day: '2-digit',
                     month: 'long',
                     year: 'numeric'
@@ -87,7 +104,7 @@ angular.module('dashboard', ['ngAnimate', 'ngMaterial', 'ngMessages'])
         $http({
             method: 'GET',
             // url: 'https://devapi.freightbro.com/api/search/5c335c0cad334f1fae13ffad'
-            // url: 'https://devapi.freightbro.com/api/schedules?from_port=INNSA&to_port=DEHAM&carrier_code=&from_date=2019-01-14'
+            // url: 'sadfasf'
             url: '/api/data'
         }).then((reponse) => {
             let resultArray = Array()
@@ -98,7 +115,7 @@ angular.module('dashboard', ['ngAnimate', 'ngMaterial', 'ngMessages'])
                     "vendor": jsonResponse.rates[i]["vendor"]["vendor_name"],
                     "subVendor": jsonResponse.rates[i]["sub_vendor"]["sub_vendor_name"],
                     "serviceType": (jsonResponse.rates[i]["service_type"] || "NA"),
-                    "expiry": timeConversion(jsonResponse.rates[i]["expiry"] || "NA"),
+                    "expiry": "Expires on " + $scope.timeConversion(jsonResponse.rates[i]["expiry"] || "NA"),
                     "legCurrencyCost": jsonResponse.rates[i]["leg_currency_cost"] || "NA",
                     "legName": jsonResponse.rates[i]["leg_name"] || "NA",
                     "totalCost": (jsonResponse.rates[i]["leg_total_cost"] || "NA"),
